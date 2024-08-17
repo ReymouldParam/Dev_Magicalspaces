133110df9f86cd3b967a026cf08aade2e1a73328
nareshnarnapati <nareshnarnapati@gmail.com>
2024-08-17 16:29:52 +0530

    Updated code of Magicalspaces

--- Assets/globaljs.js
+++ Assets/globaljs.js
@@ -2,17 +2,14 @@ window.addEventListener('scroll', function () {
     var navbar = document.querySelector('header');
     var navlist = document.querySelector('.nav-list-options');
     var sociallist = document.querySelector('.socialmedia-section');
+    if (window.scrollY > 3) {
+        navbar.classList.add('header2');
+        navlist.classList.add('nav-list-options2');
+        sociallist.classList.add('socialmedia-section2');
+    } else {
+        navbar.classList.remove('header2');
+        navlist.classList.remove('nav-list-options2');
+        sociallist.classList.remove('socialmedia-section2');
 
-    // Ensure elements are found before attempting to modify classes
-    if (navbar && navlist && sociallist) {
-        if (window.scrollY > 3) {
-            navbar.classList.add('header2');
-            navlist.classList.add('nav-list-options2');
-            sociallist.classList.add('socialmedia-section2');
-        } else {
-            navbar.classList.remove('header2');
-            navlist.classList.remove('nav-list-options2');
-            sociallist.classList.remove('socialmedia-section2');
-        }
     }
 });
