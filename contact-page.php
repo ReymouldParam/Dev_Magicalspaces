f36bd31ccec4e86d4db5af1ab8af7c9c3aaf1bc7
Revanth D <revanth.danduboina@reymould.com>
2025-11-14 16:50:22 +0530

    images, htaccess, and mail integratio completed

---
+++ contact-page.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Collect data from form
+    $name = $_POST['name'];
+    $number = $_POST['number'];
+    $email = $_POST['email'];
+    $location = $_POST['location'];
+    $service = $_POST['service'];
+    $message = $_POST['message'];
+
+    // Recipient email
+    $to = "sales@themagicalspaces.com";
+    // Email subject and body
+    $subject = "Email Enquiry from Magical Spaces Website";
+    $body = "Name: $name\n"
+        . "Email: $email\n"
+        . "Phone Number: $number\n"
+        . "Location: $location\n"
+        . "Project Type: $service\n"
+        . "Project Requirements:\n$message";
+
+    // Send the email
+    $emailSent = mail($to, $subject, $body);
+
+    // Redirect based on result
+    if ($emailSent) {
+        header("Location: contact?emailSuccess=true");
+    } else {
+        header("Location: contact?emailSuccess=false");
+    }
+    exit;
+}
+?>
